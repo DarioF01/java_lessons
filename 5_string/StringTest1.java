22c8b3a0c16ba1a16201f28759b34913c3dd31b2
Dario Fabricius <andre.fabricius@gmail.com>
2024-07-11 14:46:26 +0200

    third commit

---
+++ 5_string/StringTest1.java
@@ -0,0 +1,10 @@
+public class StringTest1{
+	public static void main(String [] args){
+		String name = "Dario";
+		String surname = "Fabricius";
+				String fullName = name+" "+surname;
+
+		System.out.println("Full Name :"+fullName);
+	}
+	
+}
